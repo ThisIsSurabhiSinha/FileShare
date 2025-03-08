76e6e975a81282e371b3c1aa825aa3ec46b18c26
ThisSurabhiSinha <surabhisinha514@gmail.com>
2025-03-08 17:21:30 +0530

    added whitenoise settings

--- fileshare/urls.py
+++ fileshare/urls.py
@@ -3,10 +3,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 from home.views import HandleFileUpload  ,home , download
+from django.conf.urls.static import static
+from django.urls import re_path
+from django.views.static import serve
 urlpatterns = [
     path('handle/', HandleFileUpload.as_view(), name='handle_upload'),  
     path('download/<uid>', download, name='download'),  
     path('admin/', admin.site.urls),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     path('',home , name ='home')
 ]
 
